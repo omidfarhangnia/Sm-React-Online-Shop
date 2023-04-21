5a8954216cad13fed03a40f81f24471a01434c5e
omidfarhangnia <83111084+omidfarhangnia@users.noreply.github.com>
2023-04-21 10:15:27 +0330

    fixig the bug and giving the value

--- src/components/CategoriesDemo.jsx
+++ src/components/CategoriesDemo.jsx
@@ -1,49 +1,16 @@
-import React, { useEffect, useState } from "react";
-import { GiveData } from "../context/AuthContext";
 
-const CategoriesDemo = ({ categoryName }) => {
-  const [items, setItems] = useState({});
-  useEffect(() => {
-    fetch("https://api.escuelajs.co/api/v1/products")
-      .then((res) => res.json())
-      .then((data) => {
-        let cleanedData = {
-          clothes: [],
-          electronics: [],
-          furniture: [],
-          shoes: [],
-          others: [],
-        };
-
-        for (var i = 0; i < data.length; i++) {
-          switch (data[i].category.name) {
-            case "Clothes":
-              cleanedData.clothes.push(data[i]);
-              break;
-            case "Electronics":
-              cleanedData.electronics.push(data[i]);
-              break;
-            case "Furniture":
-              cleanedData.furniture.push(data[i]);
-              break;
-            case "Shoes":
-              cleanedData.shoes.push(data[i]);
-              break;
-            case "Others":
-              cleanedData.others.push(data[i]);
-              break;
-            default:
-          }
-        }
-
-        setItems(cleanedData);
-      });
-  }, []);
-
+const CategoriesDemo = ({ categoryName, items }) => {
   return (
     <div>
       <h1 className="text-3xl font-bold">{categoryName}</h1>
       <div>
+      {items[categoryName]?.map((item, index) => {
+        if(item === undefined) {
+          return "";
+        }else{
+          return(<li key={index}>{item.title}</li>)
+        }
+      })}
       </div>
     </div>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,62 @@
+import { useEffect, useState } from "react";
 import Categories from "../components/Categories";
 import CategoriesDemo from "../components/CategoriesDemo";
 
-
 const Home = () => {
+  const [items, setItems] = useState({
+    clothes: [],
+    electronics: [],
+    furniture: [],
+    shoes: [],
+    others: [],
+  });  
+
+  useEffect(() => {
+    fetch("https://api.escuelajs.co/api/v1/products")
+      .then((res) => res.json())
+      .then((data) => {
+        let cleanedData = {
+          clothes: [],
+          electronics: [],
+          furniture: [],
+          shoes: [],
+          others: [],
+        };
+
+
+        for (var i = 0; i < data.length; i++) {
+          switch (data[i].category.name) {
+            case "Clothes":
+              cleanedData.clothes.push(data[i]);
+              break;
+            case "Electronics":
+              cleanedData.electronics.push(data[i]);
+              break;
+            case "Furniture":
+              cleanedData.furniture.push(data[i]);
+              break;
+            case "Shoes":
+              cleanedData.shoes.push(data[i]);
+              break;
+            case "Others":
+              cleanedData.others.push(data[i]);
+              break;
+            default:
+          }
+        }
+
+        setItems(cleanedData);
+      });
+  }, []);
+
   return (
     <>
       <Categories />
-      <CategoriesDemo categoryName={"clothes"} />
-      <CategoriesDemo categoryName={"electronics"} />
-      <CategoriesDemo categoryName={"furniture"} />
-      <CategoriesDemo categoryName={"shoes"} />
-      <CategoriesDemo categoryName={"others"} />
+      <CategoriesDemo categoryName={"clothes"} items={items} />
+      <CategoriesDemo categoryName={"electronics"} items={items} />
+      <CategoriesDemo categoryName={"furniture"} items={items} />
+      <CategoriesDemo categoryName={"shoes"} items={items} />
+      <CategoriesDemo categoryName={"others"} items={items} />
     </>
   );
 };
