30eb405d5e29b01728925c599c1f9e81ff4d2a98
omidfarhangnia <83111084+omidfarhangnia@users.noreply.github.com>
2023-04-21 23:41:51 +0330

    i work on a new level

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, json } from "react-router-dom";
 import Home from "./pages/Home";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import AuthContextProvider from "./context/AuthContext";
+import { useEffect } from "react";
+import { addDoc, collection, onSnapshot, query } from "firebase/firestore";
+import { db } from "./firebase";
 
 function App() {
+  useEffect(() => {
+    const q = query(collection(db, "items"));
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      console.log(querySnapshot)
+      //       querySnapshot.forEach((doc) => {
+      // console.log(doc)
+      // });
+    });
+    return () => unsubscribe();
+  }, []);
   return (
     <>
       <AuthContextProvider>
--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,17 +1,15 @@
-import React, { createContext, useContext, useEffect, useState } from "react";
+import React, { createContext, useContext, useEffect } from "react";
 
 const AuthContext = createContext();
 
 const AuthContextProvider = ({ children }) => {
   return (
-    <AuthContext.Provider value={{}}>
-        {children}
-    </AuthContext.Provider>
+    <AuthContext.Provider value={{}}>{/* {children} */}</AuthContext.Provider>
   );
 };
 
 export default AuthContextProvider;
 
 export function GiveData() {
-    return useContext(AuthContext);
+  return useContext(AuthContext);
 }
