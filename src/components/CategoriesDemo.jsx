b893039028c75ecdbf77bf49724af96333c26e5d
omidfarhangnia <83111084+omidfarhangnia@users.noreply.github.com>
2023-04-22 14:51:17 +0330

    adding demo

--- src/components/CategoriesDemo.jsx
+++ src/components/CategoriesDemo.jsx
@@ -1,10 +1,20 @@
+import { GiveData } from "../context/AuthContext";
 
-const CategoriesDemo = ({ categoryName, items }) => {
+const CategoriesDemo = ({ categoryName }) => {
+  const { items } = GiveData();
+  const filteredItem = items.filter((item) => item.category === categoryName);
+
   return (
-    <div>
-      <h1 className="text-3xl font-bold">{categoryName}</h1>
-      <div>
-      </div>
+    <div className="flex flex-wrap justify-between">
+      <h1 className="text-3xl font-bold w-[100vw] text-center">{categoryName}</h1>
+      {filteredItem.map((item) =>  (
+        <div className="w-[30%] select-none">
+          <img className="object-cover object-top w-[80%] h-[60%]" src={item.imgPath} alt={item.description}/>
+          <h5>{item.title}</h5>
+          <div>{item.price}</div>
+          <div>{item.score}</div>
+        </div>
+      ))}
     </div>
   );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,15 @@
 import { useEffect, useState } from "react";
 import Categories from "../components/Categories";
 import CategoriesDemo from "../components/CategoriesDemo";
-import { GiveData } from "../context/AuthContext";
 
+const Home = () => {
 
-const Home = () => {  
-  const {items} = GiveData();
-
-  console.log(items)
   return (
     <>
       <Categories />
-      {/* <CategoriesDemo categoryName={"clothes"} items={items} />
-      <CategoriesDemo categoryName={"electronics"} items={items} />
-      <CategoriesDemo categoryName={"furniture"} items={items} />
-      <CategoriesDemo categoryName={"shoes"} items={items} />
-      <CategoriesDemo categoryName={"others"} items={items} /> */}
+      <CategoriesDemo categoryName={"women"} items={""} />
+      <CategoriesDemo categoryName={"man"} items={""} />
+      <CategoriesDemo categoryName={"baby"} items={""} />
     </>
   );
 };
